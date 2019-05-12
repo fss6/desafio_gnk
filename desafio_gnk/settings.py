f65918dc78529cfd49947a7190560e6d5c105289
Fábio Santos <fabiosantos.pe@gmail.com>
2019-05-11 23:35:25 -0300

    Heroku config v3

--- desafio_gnk/settings.py
+++ desafio_gnk/settings.py
@@ -138,8 +138,8 @@ task_queues = (
     )
 )
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = os.getenv('REDIS_URL', 'redis://localhost:6379')
+CELERY_RESULT_BACKEND = os.getenv('REDIS_URL', 'redis://localhost:6379')
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
